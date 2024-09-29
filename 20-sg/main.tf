af8654a863613a3d5f32c25a9b45af6cb9fb8f6e
jagadeesh <jagadeeshakula123@gmail.com>
2024-09-30 00:18:35 +0530

    expense-terraform-dev

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -1,9 +1,9 @@
 module "mysql_sg" {
-  source = "../../security-group-module"
-  project = var.project
+  source      = "../../security-group-module"
+  project     = var.project
   environment = var.environment
-  sg_name = "mysql"
-  vpc_id = local.vpc_id
+  sg_name     = "mysql"
+  vpc_id      = local.vpc_id
   common_tags = var.common_tags
-  sg_tags = var.mysql_sg_tags
+  sg_tags     = var.mysql_sg_tags
 }
