9ab0c9bd3b5f9a9437fdba27af70e5ec98d2fcec
jagadeesh <jagadeeshakula123@gmail.com>
2024-12-18 21:21:29 +0530

    expense-infra-dev

---
+++ 20-sg/variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+  default = "expense"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Terraform   = true
+    Environment = "Dev"
+  }
+}
+variable "mysql_sg_tags" {
+    default = {
+        Component = "mysql"
+    }
+}
