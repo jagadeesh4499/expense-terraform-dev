c008fbeac58d91b81a2bdb4038f16075cf9b412a
jagadeesh <jagadeeshakula123@gmail.com>
2024-12-19 22:09:09 +0530

    expense-terraform-dev

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -34,6 +34,15 @@ module "bastion_sg" {
   common_tags  = var.common_tags
   sg_tags      = var.bastion_sg_tags
 }
+module "ansible_sg" {
+  source       = "../../terraform-aws-security-group"
+  project_name = var.project_name
+  environment  = var.environment
+  sg_name      = "ansible"
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+  sg_tags      = var.ansible_sg_tags
+}
 # MysQl allowing connections on 3306 from the instances attached to backend SG
 resource "aws_security_group_rule" "mysql_backend" {
   type                     = "ingress"
@@ -82,4 +91,44 @@ resource "aws_security_group_rule" "frontend_bastion" {
   protocol                 = "tcp"
   source_security_group_id = module.bastion_sg.id
   security_group_id        = module.frontend_sg.id
+}
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.id
+}
+resource "aws_security_group_rule" "mysql_ansible" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.ansible_sg.id
+  security_group_id        = module.mysql_sg.id
+}
+resource "aws_security_group_rule" "backend_ansible" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.ansible_sg.id
+  security_group_id        = module.backend_sg.id
+}
+resource "aws_security_group_rule" "frontend_ansible" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.ansible_sg.id
+  security_group_id        = module.frontend_sg.id
+}
+resource "aws_security_group_rule" "ansible_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.ansible_sg.id
 }
--- 40-apps/variables.tf
+++ 40-apps/variables.tf
@@ -11,28 +11,26 @@ variable "common_tags" {
     Environment = "Dev"
   }
 }
-variable "mysql_sg_tags" {
-  default = {
-    Component = "mysql"
-  }
-}
-variable "backend_sg_tags" {
-  default = {
-    Component = "backend"
-  }
-}
-variable "frontend_sg_tags" {
+variable "frontend_tags" {
   default = {
     Component = "frontend"
   }
 }
-variable "bastion_sg_tags" {
+variable "backend_tags" {
   default = {
-    Component = "bastion"
+    Component = "backend"
   }
 }
-variable "ansible_sg_tags" {
+variable "mysql_tags" {
+  default = {
+    Component = "mysql"
+  }
+}
+variable "ansible_tags" {
   default = {
     Component = "ansible"
   }
+}
+variable "zone_name" {
+  default = "jagadeesh.online"
 }
